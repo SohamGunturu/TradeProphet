2cb4922517379037d06df3081188eeba13d79df2
FarhanSadeek21 <farhansadeek019@gmail.com>
2023-03-25 12:25:17 -0400

    Price today

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ st.title('Trade Prophet')
 # Create the container
 
 sidebar = st.container()
-dataset = st.container()
+dfset = st.container()
 open_close = st.container()
 high_low = st.container()
 volume = st.container()
-
+change = st.container()
 
 # Sidebar
 with sidebar:
@@ -28,7 +28,7 @@ with sidebar:
     start = st.sidebar.date_input('Enter Start Date', date.today() - timedelta(days=100))
     end = st.sidebar.date_input('Enter End Date')
 
-# Dataset
+# dfset
 df = yf.download(ticker, start=start, end=end)
 
 # Volume
@@ -45,3 +45,11 @@ with open_close:
 with high_low:
     fig_hl = px.line(df, x = df.index, y = ["High", "Low"], title = "High - Low")
     st.plotly_chart(fig_hl)
+
+# Change
+
+with change:
+    close_now = round(float(str(df.tail(1)['Close'].values[0])), 2)
+    open_now = round(float(str(df.tail(1)['Open'].values[0])), 2)
+    high_now = round(float(str(df.tail(1)['High'].values[0])), 2)
+    low_now = round(float(str(df.tail(1)['Low'].values[0])), 2)
