dba869c9da9f448c17c5db19e1e420d04eb43b69
FarhanSadeek21 <farhansadeek019@gmail.com>
2023-03-25 12:34:49 -0400

    Fixed Old Code

--- app.py
+++ app.py
@@ -24,12 +24,13 @@ with sidebar:
     if market == 'NYSE':
         ticker = st.sidebar.selectbox('Enter Ticker', ('C', 'JPM', 'WFC', 'BAC')   )
     elif market == 'NASDAQ':
-        ticker = st.sidebar.selectbox('Enter Ticker', ('AAPL', 'MSFT', 'GOOG', 'AMZN', 'FB', 'GOOG'))
+        ticker = st.sidebar.selectbox('Enter Ticker', ('AAPL', 'MSFT', 'GOOG', 'AMZN', 'FB', 'GOOG', 'AMD'))
     start = st.sidebar.date_input('Enter Start Date', date.today() - timedelta(days=100))
     end = st.sidebar.date_input('Enter End Date')
 
 # dfset
 df = yf.download(ticker, start=start, end=end)
+df = df[::-1]
 
 # Volume
 with volume:
@@ -49,26 +50,28 @@ with high_low:
 # Change
 
 with change:
-    close_now = round(float(str(df.tail(1)['Close'].values[0])), 2)
-    open_now = round(float(str(df.tail(1)['Open'].values[0])), 2)
-    high_now = round(float(str(df.tail(1)['High'].values[0])), 2)
-    low_now = round(float(str(df.tail(1)['Low'].values[0])), 2)
 
-    close_past = round(float(str(df.head(1)['Close'].values[0])), 2)
-    open_past = round(float(str(df.head(1)['Open'].values[0])), 2)
-    high_past = round(float(str(df.head(1)['High'].values[0])), 2)
-    low_past = round(float(str(df.head(1)['Low'].values[0])), 2)
+    close_now = round(float(str(df.head(1)['Close'].values[0])), 2)
+    open_now = round(float(str(df.head(1)['Open'].values[0])), 2)
+    high_now = round(float(str(df.head(1)['High'].values[0])), 2)
+    low_now = round(float(str(df.head(1)['Low'].values[0])), 2)
 
-    close_change = round(close_past - close_now, 2)
-    open_change = round(open_past - open_now, 2)
-    high_change = round(high_past - high_now, 2)
-    low_change = round(low_past - low_now, 2)
     
+    close_past = round(float(str(df.tail(1)['Close'].values[0])), 2)
+    open_past = round(float(str(df.tail(1)['Open'].values[0])), 2)
+    high_past = round(float(str(df.tail(1)['High'].values[0])), 2)
+    low_past = round(float(str(df.tail(1)['Low'].values[0])), 2)
+
+    close_change = round(close_now - close_past, 2)
+    open_change = round(open_now - open_past, 2)
+    high_change = round(high_now - high_past, 2)
+    low_change = round(low_now - low_past, 2)
+
     close_percent = round(close_change / close_past * 100, 2)
     open_percent = round(open_change / open_past * 100, 2)
     high_percent = round(high_change / high_past * 100, 2)
     low_percent = round(low_change / low_past * 100)
 
     col1, col2 = st.columns(2)
-    col1.metric('Close', str(close_now), str(close_change) +'(' + str(close_percent) + '%)')
+    col1.metric('Close', str(close_now), str(close_change) +' (' + str(close_percent) + '%)')
     col2.metric('Open', str(open_now), str(open_change) + ' (' + str(open_percent) + '%)')
