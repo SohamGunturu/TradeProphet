97f486e87ff211461c62d47a8bdbb9f176c94bcc
FarhanSadeek21 <farhansadeek019@gmail.com>
2023-03-25 11:34:28 -0400

    dataset and volume

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import yfinance as yf
 import pandas as pd
 import plotly.express as px
 import numpy as np
+from datetime import datetime, timedelta, date
 
 st.set_page_config(layout="wide")
 st.title('Trade Prophet')
@@ -10,8 +11,13 @@ st.title('Trade Prophet')
 # Create the container
 
 sidebar = st.container()
+dataset = st.container()
+open_close = st.container()
+high_low = st.container()
+volume = st.container()
 
-# 
+
+# Sidebar
 with sidebar:
     st.sidebar.title('Options')
     market = st.sidebar.selectbox('Enter Market', np.array(['NYSE', 'NASDAQ']))
@@ -19,5 +25,12 @@ with sidebar:
         ticker = st.sidebar.selectbox('Enter Ticker', ('C', 'JPM', 'WFC', 'BAC')   )
     elif market == 'NASDAQ':
         ticker = st.sidebar.selectbox('Enter Ticker', ('AAPL', 'MSFT', 'GOOG', 'AMZN', 'FB', 'GOOG'))
+    start = st.sidebar.date_input('Enter Start Date', date.today() - timedelta(days=365))
+    end = st.sidebar.date_input('Enter End Date')
 
-    
+# Dataset
+df = yf.download(ticker, start=start, end=end)
+
+with volume:
+    fig_vol = px.area(df, x=df.index, y="Volume", title='Volume')
+    st.plotly_chart(fig_vol)
