4178ed911f53aaecb7faf7f86724b596f6453932
FarhanSadeek21 <farhansadeek019@gmail.com>
2023-03-25 10:57:20 -0400

    Market Sidebar

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import plotly.express as px
+import numpy as np
 
 st.set_page_config(layout="wide")
 st.title('Trade Prophet')
+
+# Create the container
+
+sidebar = st.container()
+
+# 
+with sidebar:
+    st.sidebar.title('Options')
+    st.sidebar.selectbox('Enter Market', np.array(['NYSE', 'NASDAQ']))
